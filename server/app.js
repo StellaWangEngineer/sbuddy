19602f6d062f7bbdfae4ebb250e8a24df451e880
Arcgo <imingjun.ma@outlook.com>
2023-06-09 20:29:49 -0700

    [feat] implement connect mongoDB in az

--- server/app.js
+++ server/app.js
@@ -3,10 +3,13 @@ var express = require('express');
 var path = require('path');
 var cookieParser = require('cookie-parser');
 var logger = require('morgan');
+const connectDB = require('./db');
 
 var groupsRouter = require('./routes/groups');
 
 var app = express();
+
+connectDB();
 
 // view engine setup
 app.set('views', path.join(__dirname, 'views'));
