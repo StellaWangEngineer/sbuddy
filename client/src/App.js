219800a20caa2bf9c36007af0c6209b23977b2f1
Arcgo <imingjun.ma@outlook.com>
2023-06-16 15:29:58 -0700

    [feat] implement header

--- client/src/App.js
+++ client/src/App.js
@@ -1,23 +1,35 @@
-import logo from './logo.svg';
+import React, { useState } from 'react';
+import { FaSun, FaMoon } from "react-icons/fa";
 import './App.css';
+import NavBar from './components/NavBar';
 
 function App() {
+  const [theme, setTheme] = useState('light');
+  const [isChecked, setIsChecked] = useState(theme === 'dark');
+
+  const toggleTheme = () => {
+    const newTheme = theme === 'light' ? 'dark' : 'light';
+    setTheme(newTheme);
+    setIsChecked(newTheme === 'dark');
+  };
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className={`App-${theme}`}>
+      <div className="App">
+        <header className="App-header">
+        <div className="nav-container">
+          <NavBar />
+        </div>
+        <div className="toggle-container">
+          <FaSun />
+          <label className="toggle-switch">
+            <input type='checkbox' checked={isChecked} onChange={toggleTheme} />
+            <span className="slider"></span>
+          </label>
+          <FaMoon />
+        </div>
+        </header>
+      </div>
     </div>
   );
 }
---
+++ client/src/components/NavBar.js
@@ -0,0 +1,12 @@
+import './NavBar.css';
+
+function NavBar(params) {
+    return (
+        <div className="navbar">
+          <div className="logo">Sbuddy</div>
+          <button className="sign-button">Sign in</button>
+        </div>
+    );
+}
+
+export default NavBar;
