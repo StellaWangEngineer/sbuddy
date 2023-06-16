4abe0eabf9dcd288730710928c4cc8a828dcceaa
Arcgo <imingjun.ma@outlook.com>
2023-06-15 17:36:16 -0700

    [refactoring] rebuild fontend

--- client/src/App.js
+++ client/src/App.js
@@ -1,21 +1,23 @@
-import logo from "./logo.svg";
-import "./App.css";
-import Navbar from "./components/Navbar";
-import SearchBar from "./components/SearchBar";
-import CategoryFilter from "./components/CategoryFilter";
+import logo from './logo.svg';
+import './App.css';
 
 function App() {
   return (
     <div className="App">
       <header className="App-header">
-        <Navbar />
+        <img src={logo} className="App-logo" alt="logo" />
+        <p>
+          Edit <code>src/App.js</code> and save to reload.
+        </p>
+        <a
+          className="App-link"
+          href="https://reactjs.org"
+          target="_blank"
+          rel="noopener noreferrer"
+        >
+          Learn React
+        </a>
       </header>
-      <div className="App-search">
-        <SearchBar />
-      </div>
-      <div className="App-content">
-        <CategoryFilter />
-      </div>
     </div>
   );
 }
