6e9290439bd17edaa6ede3d9dda1ed9613f366aa
Arcgo <imingjun.ma@outlook.com>
2023-06-20 13:58:10 -0700

    [feat] CORS

--- server/app.js
+++ server/app.js
@@ -26,6 +26,14 @@ app.use(express.urlencoded({ extended: false }));
 app.use(cookieParser());
 app.use(express.static(path.join(__dirname, 'public')));
 
+// CORS
+app.use((req, res, next) => {
+  res.setHeader('Access-Control-Allow-Origin', '*');
+  res.setHeader('Access-Control-Allow-Methods', 'GET, POST, PUT, DELETE');
+  res.setHeader('Access-Control-Allow-Headers', 'Content-Type');
+  next();
+});
+
 app.use('/groups', groupsRouter);
 
 // catch 404 and forward to error handler
